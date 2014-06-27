210336add1618dd6be73b96181c496daee90fae2
Adi Sivasankaran <adi.sivasankaran@gmail.com>
2014-06-27 15:33:51 -0400

    Set up the basic Backstretch with one image.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,15 @@
+
+
 'use strict';
 
-angular.module('galleryFsApp', [
+var app = angular.module('galleryFsApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
   'ngRoute'
-])
-  .config(function ($routeProvider, $locationProvider, $httpProvider) {
+]);
+
+app.config(function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'partials/main',
@@ -28,9 +31,9 @@ angular.module('galleryFsApp', [
       .otherwise({
         redirectTo: '/'
       });
-      
+
     $locationProvider.html5Mode(true);
-      
+
     // Intercept 401s and redirect you to login
     $httpProvider.interceptors.push(['$q', '$location', function($q, $location) {
       return {
@@ -45,14 +48,15 @@ angular.module('galleryFsApp', [
         }
       };
     }]);
-  })
-  .run(function ($rootScope, $location, Auth) {
+  });
 
+app.run(function ($rootScope, $location, Auth) {
     // Redirect to login if route requires auth and you're not logged in
     $rootScope.$on('$routeChangeStart', function (event, next) {
-      
+
       if (next.authenticate && !Auth.isLoggedIn()) {
         $location.path('/login');
       }
     });
   });
+
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,22 @@
+
+
 'use strict';
 
-angular.module('galleryFsApp')
-  .controller('MainCtrl', function ($scope, $http) {
+var app = angular.module('galleryFsApp');
+
+app.controller('MainCtrl', function ($scope, $http) {
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
-  });
+});
+
+app.directive('turnred', function() {
+    return function(scope, element) {
+        element.bind('mouseover', function(){
+            element.addClass('turn-red');
+        });
+        element.bind('mouseleave', function() {
+            element.removeClass('turn-red');
+        });
+    };
+});
