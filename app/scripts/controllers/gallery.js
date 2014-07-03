b068109847dfdba20f26d5fe667ccaa454577255
Adi Sivasankaran <adi.sivasankaran@gmail.com>
2014-07-03 13:23:46 -0400

    LinkedIn Auth complete.  Set up grid system.  Sidebar in position.  Move on to forms & adding project schema.

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -5,7 +5,7 @@
 var app = angular.module('galleryFsApp');
 
 app.controller('GalleryCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    // $http.get('/api/awesomeThings').success(function(awesomeThings) {
+    //   $scope.awesomeThings = awesomeThings;
+    // });
 });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,13 +10,3 @@ app.controller('MainCtrl', function ($scope, $http) {
     });
 });
 
-app.directive('turnred', function() {
-    return function(scope, element) {
-        element.bind('mouseover', function(){
-            element.addClass('turn-red');
-        });
-        element.bind('mouseleave', function() {
-            element.removeClass('turn-red');
-        });
-    };
-});
