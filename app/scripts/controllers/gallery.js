e040032877566e7df7738a90df480329a2e81bc3
Adi Sivasankaran <adi.sivasankaran@gmail.com>
2014-07-05 23:11:51 -0400

    Add projects to gallery controller.

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -5,13 +5,27 @@
 var app = angular.module('galleryFsApp');
 
 app.controller('GalleryCtrl', function ($scope, $http) {
-    $scope.images = [
-        'images/wall-pearl-1.jpg',
-        'images/manhattan-1.jpg',
-        'images/stone-st-1.jpg',
-        'images/broad-st-1.jpg',
-        'images/brooklyn-1.jpg',
-        'images/wall-pearl-1.jpg',
-        'images/broad-st-2.jpg',
+    $scope.projects = [
+        {
+            'images/wall-pearl-1.jpg'
+        },
+        {
+            'images/manhattan-1.jpg'
+        },
+        {
+            'images/stone-st-1.jpg'
+        },
+        {
+            'images/broad-st-1.jpg'
+        },
+        {
+            'images/brooklyn-1.jpg'
+        },
+        {
+            'images/wall-pearl-1.jpg'
+        },
+        {
+            'images/broad-st-2.jpg'
+        },
     ];
 });
