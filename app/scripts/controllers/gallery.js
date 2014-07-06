361a267b953ed20cd43ee72648ba0aeed08793f1
Adi Sivasankaran <adi.sivasankaran@gmail.com>
2014-07-05 22:29:27 -0400

    Add more images to the images directory.

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -5,7 +5,13 @@
 var app = angular.module('galleryFsApp');
 
 app.controller('GalleryCtrl', function ($scope, $http) {
-    // $http.get('/api/awesomeThings').success(function(awesomeThings) {
-    //   $scope.awesomeThings = awesomeThings;
-    // });
+    $scope.images = [
+        'images/wall-pearl-1.jpg',
+        'images/manhattan-1.jpg',
+        'images/stone-st-1.jpg',
+        'images/broad-st-1.jpg',
+        'images/brooklyn-1.jpg',
+        'images/wall-pearl-1.jpg',
+        'images/broad-st-2.jpg',
+    ];
 });
