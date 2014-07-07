fcc4e5a2ea8fcfba929a33f161ccc22171822f9e
Adi Sivasankaran <adi.sivasankaran@gmail.com>
2014-07-07 17:32:05 -0400

    Set up ng-repeat with the seed-data and populate gallery.htmt with project screenshots.

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -8,27 +8,27 @@ app.controller('GalleryCtrl', function ($scope, $http) {
     $http.get('/api/getProjectData').success(function(projectData){
         $scope.projectData = projectData;
     });
-    $scope.projects = [
-        {
-            imageUrl:'images/wall-pearl-1.jpg'
-        },
-        {
-            imageUrl:'images/manhattan-1.jpg'
-        },
-        {
-            imageUrl:'images/stone-st-1.jpg'
-        },
-        {
-            imageUrl:'images/broad-st-1.jpg'
-        },
-        {
-            imageUrl:'images/brooklyn-1.jpg'
-        },
-        {
-            imageUrl:'images/wall-pearl-1.jpg'
-        },
-        {
-            imageUrl:'images/broad-st-2.jpg'
-        }
-    ];
+    // $scope.projects = [
+    //     {
+    //         imageUrl:'images/wall-pearl-1.jpg'
+    //     },
+    //     {
+    //         imageUrl:'images/manhattan-1.jpg'
+    //     },
+    //     {
+    //         imageUrl:'images/stone-st-1.jpg'
+    //     },
+    //     {
+    //         imageUrl:'images/broad-st-1.jpg'
+    //     },
+    //     {
+    //         imageUrl:'images/brooklyn-1.jpg'
+    //     },
+    //     {
+    //         imageUrl:'images/wall-pearl-1.jpg'
+    //     },
+    //     {
+    //         imageUrl:'images/broad-st-2.jpg'
+    //     }
+    // ];
 });
