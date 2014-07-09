7d0b4c07441e588153ff1fa3f03c9e872f6fc21e
Adi Sivasankaran <adi.sivasankaran@gmail.com>
2014-07-09 17:26:40 -0400

    Add bounceIn & fadeIn animations to project.html.

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -8,17 +8,24 @@ app.controller('GalleryCtrl', function ($scope, $http, $location) {
     $http.get('/api/getProjectData').success(function(projectData){
         $scope.projectData = projectData;
     });
-
+    $scope.search = '';
     $scope.$location = $location;
-
-    var displayProject = function(){
-        for(var i = 0; i < $scope.projectData.length; i++){
-            if($scope.projectData[i].screenshotLink !== ''){
-                return true;
+    $scope.searchFunc = function(proj){
+        var lowerInput = $scope.search.toLowerCase();
+        // Create boolean value:
+        var text_val = true;
+        var tech_tags = proj.techTags.join('');
+        var proj_text = ('' + proj.projectName + proj.description + proj.pitch + tech_tags).toLowerCase();
+        if(lowerInput.length > 0){
+            if(proj_text.indexOf(lowerInput) > -1){
+                console.log(proj);
+                text_val = true;
             } else{
-                return false;
+                text_val = false;
             }
         }
+
+        return text_val;
     }
 });
 
